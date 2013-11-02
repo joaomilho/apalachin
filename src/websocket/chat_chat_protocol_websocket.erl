9eb20f886191e15267c2839307df9581884346f4
Maiz Lulkin <maiz@lulk.in>
2013-11-03 01:37:28 +0200

    Refactor chat code

--- src/websocket/chat_chat_protocol_websocket.erl
+++ src/websocket/chat_chat_protocol_websocket.erl
@@ -4,92 +4,85 @@
 -record(state, {users}).
 
 -compile(export_all).
+-import(ws_lib, [notify_all/2, notify_all_except_me/3]).
 
-user_id(Sess) ->
-  boss_session:get_session_data(binary_to_list(Sess), user_id).
-
-init() ->
-  {ok, #state{users=dict:new()}}.
+% helpers
 
 user_list(Users) ->
   lists:map(fun(X) -> list_to_binary(X) end, dict:fetch_keys(Users)).
 
-values(Dict) ->
-  list_values(dict:to_list(Dict)).
-
-list_values(List) ->
-  lists:map(fun({_,V}) -> V end, List).
-
-values_except_key(Key, Dict) ->
-  FilteredList = lists:filter(fun({K,_}) -> Key =/= K end, dict:to_list(Dict)),
-  list_values(FilteredList).
-
-notify_all(Notification, #state{users=Users}) ->
-  Connections = values(Users),
-  EncodedNotification = jsx:encode(Notification),
-  lists:map(fun(Conn) -> Conn ! {text, EncodedNotification} end, Connections).
-
-notify_all_except_me(Notification, User, #state{users=Users}) ->
-  Connections = values_except_key(User:id(), Users),
-  EncodedNotification = jsx:encode(Notification),
-  lists:map(fun(Conn) -> Conn ! {text, EncodedNotification} end, Connections).
-
 save_message(User, MessageText) ->
   Message = message:new(id, User:id(), MessageText, erlang:now()),
+  error_logger:info_msg("(Message) ~p~n", [Message]),
+
   Message:save().
 
+% protocol methods
+
+init() ->
+  {ok, #state{users=dict:new()}}.
+
 handle_join(_, WebSocketId, SessionId, State) ->
-  case SessionId of
+  error_logger:info_msg("(join) ~p~n", [State]),
+
+  case auth_lib:find_user_by_session(SessionId) of
     undefined ->
       {stop, "Auth", State};
-    SessionIdOk ->
-      case user_id(SessionId) of
-        undefined ->
-          {stop, "Auth", State};
-        UserId ->
-          case boss_db:find(UserId) of
-            undefined ->
-              {stop, "Auth", State};
-            User ->
-              #state{users=Users} = State,
-              UpdatedUsers = dict:store(UserId, WebSocketId, Users),
-              AfterJoinState = #state{users=UpdatedUsers},
+    User ->
+      #state{users=Users} = State,
+      UpdatedUsers = dict:store(User:id(), WebSocketId, Users),
+      AfterJoinState = #state{users=UpdatedUsers},
 
-              Notification = [<<"user_list">>, user_list(UpdatedUsers)],
-              notify_all(Notification, AfterJoinState),
-              {reply, ok, AfterJoinState}
-      end
-    end
+      Notification = [<<"user_list">>, user_list(UpdatedUsers)],
+      notify_all(Notification, AfterJoinState),
+      {reply, ok, AfterJoinState}
   end.
 
 handle_close(ServiceName, WebSocketId, SessionId, State) ->
-    {reply, ok, State}.
+  error_logger:info_msg("(close) ~p~n", [State]),
+
+  {reply, ok, State}.
 
 handle_incoming(_, WebSocketId, SessionId, Message, State) ->
-    UserId = user_id(SessionId),
-    case boss_db:find(UserId) of
-      undefined ->
-        {stop, "Auth", State};
-      User ->
-        Notification = case Message of
-          <<"typing">> ->
-            Data = list_to_binary(User:id()),
-            [<<"typing">>, Data];
-          _ ->
-           Data = [list_to_binary(User:id()), Message],
-           save_message(User, Message),
-            [<<"message">>, Data]
-        end,
+  error_logger:info_msg("(incoming) ~p| ~p~n", [Message, State]),
 
-        notify_all_except_me(Notification, User, State),
+  case auth_lib:find_user_by_session(SessionId) of
+    undefined ->
+      {stop, "Auth", State};
+    User ->
+      Notification = case Message of
+        <<"typing">> ->
+          Data = list_to_binary(User:id()),
+          [<<"typing">>, Data];
+        _ ->
+         Data = [list_to_binary(User:id()), Message],
+         save_message(User, Message),
+          [<<"message">>, Data]
+      end,
 
-        {noreply, State}
-    end.
+      notify_all_except_me(Notification, User, State),
 
-
-handle_info(ping,  State) -> {noreply, State};
-handle_info(state, State) -> {noreply, State};
-handle_info(_,     State) -> {noreply, State}.
+      {noreply, State}
+  end.
 
 
-terminate(Reason, State) -> ok.
+handle_info(ping,  State) ->
+  error_logger:info_msg("(info1) ~p~n", [State]),
+
+  {noreply, State};
+
+handle_info(state, State) ->
+  error_logger:info_msg("(info2) ~p~n", [State]),
+
+  {noreply, State};
+
+handle_info(_,     State) ->
+  error_logger:info_msg("(info3) ~p~n", [State]),
+
+  {noreply, State}.
+
+
+terminate(Reason, State) ->
+  error_logger:info_msg("(terminate) ~p | ~p~n", [Reason, State]),
+
+  ok.
